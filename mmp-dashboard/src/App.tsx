63f577e27078321e06379e39fe02324c1eb100d8
nikitha <nikithasanthosh2005@gmail.com>
2025-06-10 18:57:50 +0530

    b -1 g

--- mmp-dashboard/src/App.tsx
+++ mmp-dashboard/src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
 import Header from './Components/Header';
 import Dashboard from './Components/Dashboard';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="app-background">
-      <div className="dashboard-panel">
+const App: React.FC = () => (
+  <>
+    
+    <div className="site-container">
+      <div className="main-content">
         <Header />
         <Dashboard />
       </div>
     </div>
-  );
-};
+  </>
+);
 
 export default App;
